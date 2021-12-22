561f3372af39bfdafc4b56b69eefbe19d938391b
lvchao <chao.lv@brgroup.com>
2021-12-22 14:09:57 +0800

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "env": {
+    "node": true
+  },
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier",
+  ],
+  "parserOptions": { 
+    "parser": "babel-eslint"
+  },
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "lf",
+      },
+    ],
+    // "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1 }],
+    // "quotes": ["error", "double"],
+    // "semi": ["error", "always", { "omitLastInOneLineBlock": true }],
+    // "arrow-spacing": ["error", { "before": true, "after": true }]
+  },
+  "globals": {
+    "node": "readonly"
+  }
+}
